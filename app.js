c0339fab43937e5e03b65fa2338617d71fc2f840
Valentyn Fomenko <fomenko.vn@gmail.com>
2020-05-12 13:10:55 +0300

    add db connection

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.listen(5000, () => console.log('App has been started!!!'));
+const PORT = config.get('port');
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('DB Connected!');
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+}
+
+start();
+
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}!`));
